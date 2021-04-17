d0e4044344a710d77bfc93d8a0441d1a2788ee78
송진하 <songjinha@songjincBookPro.kornet>
2021-04-17 14:58:33 +0900

    git

---
+++ src/main/java/com/example/coder1/Coder1Application.java
@@ -0,0 +1,13 @@
+package com.example.coder1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Coder1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Coder1Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/coder1/Coder1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.coder1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Coder1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
